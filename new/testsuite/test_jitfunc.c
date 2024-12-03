451f28bb301c4d0425f93d3751ea87a8e0497985
Chill Magic <hanlengmowang@hotmail.com>
2024-12-04 02:08:05 +0800

    temp

--- new/testsuite/test_jitfunc.c
+++ new/testsuite/test_jitfunc.c
@@ -9,6 +9,10 @@ int add(int x, int y) {
 
 typedef int func(int, int);
 
+#include "../jitcode_impl/x86_64.h"
+
+#include <stdio.h>
+
 int main() {
     size_t size = 20 * sizeof(uint32_t);
     int r = 0;
@@ -24,6 +28,17 @@ int main() {
 
     r = jitfunc_free(the_func);
     assert(r == 0);
+
+
+    jitfunc new_func = jitfunc_alloc(0x10000);
+    assert(new_func != NULL);
+    size_t s = jitcode_mov_r64_imm64_x86_64(new_func, r64_x86_64_rax, 12345);
+    ((uint8_t*)new_func)[s] = 0xc3;
+    r = jitfunc_set_executable(the_func);
+    assert(r == 0);
+    typedef int func_1();
+    func_1 *ff = (func_1*)new_func;
+    printf("%d\n", ff());
 
     return 0;
 }
